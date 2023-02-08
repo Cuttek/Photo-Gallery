bcea147b2408f00a7976d9752c9774a1db88b303
Cuttek <31141026+Cuttek@users.noreply.github.com>
2023-02-08 18:04:49 +0100

    Add project files.

---
+++ PhotoGalleryApp/Data/GalleryParameters.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhotoGalleryApp.Data
+{
+    public class GalleryParameters
+    {
+        public GalleryParameters() { }
+        public GalleryParameters(string query, int imageQuantity, int minWidth, int maxWidth, int minHeight, int maxHeight)
+        {
+            Query = query;
+            ImageQuantity = imageQuantity;
+            MinWidth = minWidth;
+            MaxWidth = maxWidth;
+            MinHeight = minHeight;
+            MaxHeight = maxHeight;
+        }
+        public string Query { get; set; }        
+        public int ImageQuantity { get; set; }
+        public int MinWidth { get; set; }
+        public int MaxWidth { get; set; }
+        public int MinHeight { get; set; }
+        public int MaxHeight { get; set; }
+        
+    }
+}
